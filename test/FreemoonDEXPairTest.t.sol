124566ae348af19a4dd030ce0a0776adbe13d003
Paddy Curé <paddy.cure@gmail.com>
2022-05-18 21:37:52 +0100

    tested burn in pair contract

--- test/FreemoonDEXPairTest.t.sol
+++ test/FreemoonDEXPairTest.t.sol
@@ -9,10 +9,10 @@ import "../src/FreemoonDEXPair.sol";
 
 
 contract TestUser {
-    function provideLiquidity(
+    function addLiquidity(
         address pair_,
         address token0_,
-        address token1_, 
+        address token1_,
         uint256 amount0,
         uint256 amount1
     ) public
@@ -21,7 +21,17 @@ contract TestUser {
         IFRC20(token1_).transfer(pair_, amount1);
         IFreemoonDEXPair(pair_).mint(address(this));
     }
+
+    function removeLiquidity(
+        address pair_,
+        uint256 liquidity
+    ) public
+    {
+        IFRC20(pair_).transfer(pair_, liquidity);
+        IFreemoonDEXPair(pair_).burn(address(this));
+    }
 }
+
 
 contract FreemoonDEXPairTest is Test {
     MockFRC759 token0;
@@ -32,13 +42,15 @@ contract FreemoonDEXPairTest is Test {
     function setUp() public {
         token0 = new MockFRC759("Token A", "TKNA");
         token1 = new MockFRC759("Token B", "TKNB");
-        token0.mint(address(this), 10 ether);
-        token1.mint(address(this), 10 ether);
-
-        pair = new FreemoonDEXPair();
-        pair.initialize(address(token0), address(token1));
-
         testUser = new TestUser();
+        pair = new FreemoonDEXPair();
+
+        token0.mint(address(this), 3 ether);
+        token1.mint(address(this), 3 ether);
+        token0.mint(address(testUser), 3 ether);
+        token1.mint(address(testUser), 3 ether);
+
+        pair.initialize(address(token0), address(token1));
     }
 
     function addLiquidity(uint256 amount0, uint256 amount1) public {
@@ -113,7 +125,41 @@ contract FreemoonDEXPairTest is Test {
         removeLiquidity(1 ether - 1000);
 
         assertReserves(1000, 1000);
-        assertEq(token0.balanceOf(address(pair)), 1000);
-        assertEq(token1.balanceOf(address(pair)), 1000);
+    }
+
+    function testBurnUnbalancedLiquidity() public {
+        addLiquidity(1 ether, 1 ether);
+
+        assertReserves(1 ether, 1 ether);
+        assertEq(pair.balanceOf(address(this)), 1 ether - 1000);
+
+        addLiquidity(2 ether, 1 ether);
+
+        assertReserves(3 ether, 2 ether);
+        assertEq(pair.balanceOf(address(this)), 2 ether - 1000);
+
+        removeLiquidity(2 ether - 1000);
+
+        assertReserves(1500, 1000);
+        assertEq(token0.balanceOf(address(this)), 3 ether - 1500);
+        assertEq(token1.balanceOf(address(this)), 3 ether - 1000);
+    }
+
+    function testBurnUnbalancedLiquidityUnowned() public {
+        testUser.addLiquidity(address(pair), address(token0), address(token1), 1 ether, 1 ether);
+
+        assertReserves(1 ether, 1 ether);
+        assertEq(pair.balanceOf(address(testUser)), 1 ether - 1000);
+
+        addLiquidity(2 ether, 1 ether);
+
+        assertReserves(3 ether, 2 ether);
+        assertEq(pair.balanceOf(address(this)), 1 ether);
+
+        removeLiquidity(1 ether);
+
+        testUser.removeLiquidity(address(pair), 1 ether - 1000);
+
+        assertReserves(1500, 1000);
     }
 }
